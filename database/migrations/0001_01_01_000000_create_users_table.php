4842b5913f2d4376252f061c9fc33bc46bb4dfdf
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-10-10 19:37:25 +0800

    feat(auth): add super admin, phone and avatar fields to user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('super_admin')->default(false);
+            $table->string('phone')->nullable();
+            $table->string('avatar_url')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
