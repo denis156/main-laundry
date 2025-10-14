ec503ec71b9d9f4785daf1250c6b0eedaf7691cb
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-10-14 17:03:13 +0800

    fix(ui): update location information

--- resources/views/livewire/landing-page/lokasi.blade.php
+++ resources/views/livewire/landing-page/lokasi.blade.php
@@ -27,8 +27,8 @@
                         <div>
                             <h3 class="text-xl font-bold text-primary mb-2">Alamat Lengkap</h3>
                             <p class="text-neutral/75 leading-relaxed">
-                                Asrama Melati<br>
-                                Kendari, Sulawesi Tenggara<br>
+                                Main Group Cabang Kendari<br>
+                                Lalolara, Kec. Kambu, Kota Kendari, Sulawesi Tenggara<br>
                                 Indonesia
                             </p>
                         </div>
@@ -106,7 +106,7 @@
             <div data-aos="fade-left" data-aos-duration="600">
                 <x-card class="bg-base-300 shadow-lg shadow-accent h-full overflow-hidden p-0">
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3980.1383363494065!2d122.51491817560236!3d-3.9919604959817736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d988da4a3243e39%3A0x8dd4558c7a5a113d!2sAsrama%20Melati!5e0!3m2!1sid!2sid!4v1759489417536!5m2!1sid!2sid"
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d525.5202664344673!2d122.51755040310795!3d-3.992127630389825!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d988d00245398a3%3A0x2d68c413fab2ecef!2sMain%20Group%20Cabang%20Kendari!5e0!3m2!1sid!2sid!4v1760431262314!5m2!1sid!2sid"
                         class="w-full h-full min-h-[400px] lg:min-h-[600px]" style="border:0;" allowfullscreen=""
                         loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </x-card>
