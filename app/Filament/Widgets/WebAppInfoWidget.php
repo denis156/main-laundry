7ed30c07b61981b7182d98caeadae33b67edb0ae
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-10-16 18:08:10 +0800

    feat(widgets): add web app info widget

---
+++ app/Filament/Widgets/WebAppInfoWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class WebAppInfoWidget extends Widget
+{
+    protected string $view = 'filament.widgets.web-app-info-widget';
+}
