cc004a0f0b719c1f9b106d1dcfb306196bed69d1
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-10-20 02:38:17 +0800

    feat(auth): allow all users to access the Filament panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,7 +67,7 @@ class User extends Authenticatable implements FilamentUser, HasAvatar
      */
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->super_admin === true;
+        return true;
     }
 
     /**
