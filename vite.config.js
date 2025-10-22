4204aa07649e1cab8bfbd333ab1694a4030a7cf3
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-10-22 23:48:09 +0800

    feat(build): add new order ringtone script to build configuration

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
             input: [
                 'resources/css/filament/admin/theme.css',
                 'resources/css/app.css',
-                'resources/js/app.js'
+                'resources/js/app.js',
+                'resources/js/newOrderRingtone.js'
             ],
             refresh: true,
         }),
