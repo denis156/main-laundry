e31f06cc313defa091c2f9605d15005198b4fafa
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-10-27 08:02:16 +0800

    style(ui): enhance navigation visual feedback

--- resources/views/components/kurir/bottomnav.blade.php
+++ resources/views/components/kurir/bottomnav.blade.php
@@ -6,12 +6,12 @@
             <span class="text-xs {{ Route::currentRouteName() === 'kurir.beranda' ? 'text-accent' : 'text-primary-content' }} font-semibold">Beranda</span>
         </a>
         <a href="{{ route('kurir.pembayaran') }}" wire:navigate class="flex-1 flex flex-col items-center gap-1">
-            <x-icon name="{{ Route::currentRouteName() === 'kurir.pembayaran' ? 'solar.wallet-money-bold' : 'solar.wallet-money-linear' }}"
-                class="h-6 {{ Route::currentRouteName() === 'kurir.pembayaran' ? 'text-accent' : 'text-primary-content' }}" />
-            <span class="text-xs {{ Route::currentRouteName() === 'kurir.pembayaran' ? 'text-accent' : 'text-primary-content' }} font-semibold">Pembayaran</span>
+            <x-icon name="{{ in_array(Route::currentRouteName(), ['kurir.pembayaran', 'kurir.pembayaran.detail']) ? 'solar.wallet-money-bold' : 'solar.wallet-money-linear' }}"
+                class="h-6 {{ in_array(Route::currentRouteName(), ['kurir.pembayaran', 'kurir.pembayaran.detail']) ? 'text-accent' : 'text-primary-content' }}" />
+            <span class="text-xs {{ in_array(Route::currentRouteName(), ['kurir.pembayaran', 'kurir.pembayaran.detail']) ? 'text-accent' : 'text-primary-content' }} font-semibold">Pembayaran</span>
         </a>
         <a href="{{ route('kurir.pesanan') }}" wire:navigate class="flex-1 flex flex-col items-center">
-            <div class="bg-accent rounded-xl p-4 mb-2 w-14 h-10 flex items-center justify-center hover:scale-110 transition-transform">
+            <div class="bg-accent rounded-xl p-4 mb-2 w-14 h-10 flex items-center justify-center {{ in_array(Route::currentRouteName(), ['kurir.pesanan', 'kurir.pesanan.detail']) ? '' : 'border-l-4 border-r-4 border-r-accent-content border-l-accent-content' }}">
                 <x-icon name="solar.bill-list-linear" class="h-6 text-accent-content" />
             </div>
         </a>
--- resources/views/components/pelanggan/bottomnav.blade.php
+++ resources/views/components/pelanggan/bottomnav.blade.php
@@ -1,29 +1,38 @@
 <footer class="fixed bottom-0 left-0 right-0 bg-primary p-2 rounded-t-2xl z-50">
     <div class="flex items-center">
         <a href="{{ route('pelanggan.beranda') }}" wire:navigate class="flex-1 flex flex-col items-center gap-1">
-            <x-icon name="{{ in_array(Route::currentRouteName(), ['pelanggan.beranda']) ? 'solar.home-bold' : 'solar.home-linear' }}"
+            <x-icon
+                name="{{ in_array(Route::currentRouteName(), ['pelanggan.beranda']) ? 'solar.home-bold' : 'solar.home-linear' }}"
                 class="h-6 {{ in_array(Route::currentRouteName(), ['pelanggan.beranda']) ? 'text-accent' : 'text-primary-content' }}" />
-            <span class="text-xs {{ in_array(Route::currentRouteName(), ['pelanggan.beranda']) ? 'text-accent' : 'text-primary-content' }} font-semibold">Beranda</span>
+            <span
+                class="text-xs {{ in_array(Route::currentRouteName(), ['pelanggan.beranda']) ? 'text-accent' : 'text-primary-content' }} font-semibold">Beranda</span>
         </a>
         <a href="{{ route('pelanggan.pesanan') }}" wire:navigate class="flex-1 flex flex-col items-center gap-1">
-            <x-icon name="{{ in_array(Route::currentRouteName(), ['pelanggan.pesanan', 'pelanggan.pesanan.detail']) ? 'solar.bill-list-bold' : 'solar.bill-list-linear' }}"
+            <x-icon
+                name="{{ in_array(Route::currentRouteName(), ['pelanggan.pesanan', 'pelanggan.pesanan.detail']) ? 'solar.bill-list-bold' : 'solar.bill-list-linear' }}"
                 class="h-6 {{ in_array(Route::currentRouteName(), ['pelanggan.pesanan', 'pelanggan.pesanan.detail']) ? 'text-accent' : 'text-primary-content' }}" />
-            <span class="text-xs {{ in_array(Route::currentRouteName(), ['pelanggan.pesanan', 'pelanggan.pesanan.detail']) ? 'text-accent' : 'text-primary-content' }} font-semibold">Pesanan</span>
+            <span
+                class="text-xs {{ in_array(Route::currentRouteName(), ['pelanggan.pesanan', 'pelanggan.pesanan.detail']) ? 'text-accent' : 'text-primary-content' }} font-semibold">Pesanan</span>
         </a>
         <a href="{{ route('pelanggan.buat-pesanan') }}" wire:navigate class="flex-1 flex flex-col items-center">
-            <div class="bg-accent rounded-xl p-4 mb-2 w-14 h-10 flex items-center justify-center hover:scale-110 transition-transform">
+            <div
+                class="bg-accent rounded-xl p-4 mb-2 w-14 h-10 flex items-center justify-center {{ in_array(Route::currentRouteName(), ['pelanggan.buat-pesanan']) ? '' : 'border-l-4 border-r-4 border-r-accent-content border-l-accent-content' }}">
                 <x-icon name="solar.add-circle-linear" class="h-6 text-accent-content" />
             </div>
         </a>
         <a href="{{ route('pelanggan.info') }}" wire:navigate class="flex-1 flex flex-col items-center gap-1">
-            <x-icon name="{{ Route::currentRouteName() === 'pelanggan.info' ? 'solar.info-circle-bold' : 'solar.info-circle-linear' }}"
+            <x-icon
+                name="{{ Route::currentRouteName() === 'pelanggan.info' ? 'solar.info-circle-bold' : 'solar.info-circle-linear' }}"
                 class="h-6 {{ Route::currentRouteName() === 'pelanggan.info' ? 'text-accent' : 'text-primary-content' }}" />
-            <span class="text-xs {{ Route::currentRouteName() === 'pelanggan.info' ? 'text-accent' : 'text-primary-content' }} font-semibold">Informasi</span>
+            <span
+                class="text-xs {{ Route::currentRouteName() === 'pelanggan.info' ? 'text-accent' : 'text-primary-content' }} font-semibold">Informasi</span>
         </a>
         <a href="{{ route('pelanggan.profil') }}" wire:navigate class="flex-1 flex flex-col items-center gap-1">
-            <x-icon name="{{ Route::currentRouteName() === 'pelanggan.profil' ? 'solar.user-circle-bold' : 'solar.user-circle-linear' }}"
+            <x-icon
+                name="{{ Route::currentRouteName() === 'pelanggan.profil' ? 'solar.user-circle-bold' : 'solar.user-circle-linear' }}"
                 class="h-6 {{ Route::currentRouteName() === 'pelanggan.profil' ? 'text-accent' : 'text-primary-content' }}" />
-            <span class="text-xs {{ Route::currentRouteName() === 'pelanggan.profil' ? 'text-accent' : 'text-primary-content' }} font-semibold">Profil</span>
+            <span
+                class="text-xs {{ Route::currentRouteName() === 'pelanggan.profil' ? 'text-accent' : 'text-primary-content' }} font-semibold">Profil</span>
         </a>
     </div>
 </footer>
