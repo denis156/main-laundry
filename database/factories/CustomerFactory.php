2aa6fa3e143cd5f7246ff9900176d72f1e0d4052
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-10-29 06:58:42 +0800

    feat(db): add default password to customer factory

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -117,6 +117,7 @@ class CustomerFactory extends Factory
             'name' => fake()->name(),
             'phone' => fake()->unique()->numerify('8##########'), // Format: 81234567890 (tanpa 0 di depan)
             'email' => fake()->optional()->safeEmail(),
+            'password' => 'password', // default password
             'district_code' => $selectedDistrict['district_code'],
             'district_name' => $selectedDistrict['district_name'],
             'village_code' => $selectedVillage['code'],
